84ca72b7a18c37eb54cfb2245d6a6452c2525621
Alex Page <alex@anpage.me>
2020-06-23 19:24:37 -0400

    Draw rubberband rect with custom drawing

--- GDAlert/in_game/tac_view/rubber_band.gd
+++ GDAlert/in_game/tac_view/rubber_band.gd
@@ -7,12 +7,16 @@ var rect = Rect2(0, 0, 0, 0)
 
 signal select_rect_finish(p1, p2)
 
-func _ready():
-	$ColorRect.hide()
+
+func _draw():
+	if (is_selecting):
+		draw_rect(rect, Color(1, 1, 1, 0.5))
+
 
 func _gui_input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT:
+			update()
 			if event.pressed:
 				rect.position = event.position
 				rect.size = Vector2(0, 0)
@@ -23,15 +27,12 @@ func _gui_input(event):
 					var p2 = rect.position + rect.size
 					emit_signal("select_rect_finish", p1, p2)
 				is_selecting = false
-				$ColorRect.hide()
 				is_mouse_down = false
 	if event is InputEventMouseMotion:
 		if is_mouse_down:
 			rect.size.x = event.position.x - rect.position.x
 			rect.size.y = event.position.y - rect.position.y
-			$ColorRect.rect_size = rect.abs().size
-			$ColorRect.rect_position = rect.abs().position
 			if !is_selecting:
 				if abs(rect.size.x) >= 4 or abs(rect.size.y) >= 4:
 					is_selecting = true
-					$ColorRect.show()
+			update()
